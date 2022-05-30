c2198d26dffeb462a669b47fe4ba66cbd627c2b1
Ben <105577567+benjbright@users.noreply.github.com>
2022-05-30 08:17:54 +0100

    Updates

---
+++ precache-manifest.e043208789fe0775ae955a11c7aea357.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "547ec6fb03fd78f804d9",
+    "url": "/capstone/static/css/main.07673466.chunk.css"
+  },
+  {
+    "revision": "547ec6fb03fd78f804d9",
+    "url": "/capstone/static/js/main.547ec6fb.chunk.js"
+  },
+  {
+    "revision": "0d4d29ae84dcacacf9b3",
+    "url": "/capstone/static/js/1.0d4d29ae.chunk.js"
+  },
+  {
+    "revision": "64b0c223d3f18225ab3a",
+    "url": "/capstone/static/js/runtime~main.64b0c223.js"
+  },
+  {
+    "revision": "71a69e147c14aebdac91fee43231207b",
+    "url": "/capstone/index.html"
+  }
+];
